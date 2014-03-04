65ebb3172bb7ba7e2a42d43cf06d378ee3f803f0
manfred.schmidbartl <manfred.schmidbartl@munichdev.com>
2014-03-04 16:11:43 +0100

    playlistcontroller

---
+++ www/public/js/playlistController.js
@@ -0,0 +1,17 @@
+/**
+ * playlistController.
+ *
+ * >>Description<<
+ *
+ * @author Manfred
+ * @date 04.03.14 - 16:05
+ * @copyright munichDev UG
+ */
+
+
+var playlistController = function () {
+
+};
+
+
+playlistController.loadedPlaylistSongs = [];
