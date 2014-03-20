80ffbf7b7786514c506e2c2c5b030e032837ed77
Norbert <nschmidbartl@gmx.de>
2014-03-20 22:12:17 +0100

    cookies :)

--- www/public/js/accountController.js
+++ www/public/js/accountController.js
@@ -17,6 +17,59 @@ accountController.userName = "";
 accountController.requestid = 1;
 accountController.showRegisterPopup = false;
 
+accountController.getCookie = function(cname)
+{
+    var name = cname + "=";
+    var ca = document.cookie.split(';');
+    for(var i=0; i<ca.length; i++)
+    {
+        var c = ca[i].trim();
+        if (c.indexOf(name)==0) return c.substring(name.length,c.length);
+    }
+    return "";
+}
+
+accountController.setCookie = function(cname,cvalue,exdays)
+{
+    var d = new Date();
+    d.setTime(d.getTime()+(exdays*24*60*60*1000));
+    var expires = "expires="+d.toGMTString();
+    document.cookie = cname + "=" + cvalue + "; " + expires;
+}
+
+
+accountController.init = function(){
+    var trylogin = function () {
+        if(mediaController.ip_token!="auth"){
+            if(mediaController.ip_token!=""){
+                var loginTokenBase64 = accountController.getCookie("loginToken");
+                var userNameBase64   = accountController.getCookie("userName");
+                if(loginTokenBase64!="" && userNameBase64!=""){
+                    var token = rsaController.rsa.encrypt(Base64.decode(loginTokenBase64));
+                    $.ajax({
+                        timeout: 30000,
+                        url: preferences.serverURL + "?loginToken=" + token+"&auth="+mediaController.ip_token,
+                        success: function (data) {
+                            if(data=="ok"){
+                                accountController.loggedIn = true;
+                                accountController.loginToken = Base64.decode(loginTokenBase64);
+                                accountController.userName   = Base64.decode(userNameBase64);
+                                accountController.requestid = 1;
+                                $scope.safeApply();
+                                uiController.styleTopButtons();
+                            }
+                        }
+                    })
+                }
+            }
+        }
+        else{
+           setTimeout(trylogin, 1000);
+        }
+    }
+    setTimeout(trylogin, 1000);
+}
+
 accountController.toggleSignInRegister = function () {
 
     accountController.showRegisterPopup = !accountController.showRegisterPopup;
@@ -41,12 +94,14 @@ accountController.logout = function () {
     var token = rsaController.rsa.encrypt(accountController.loginToken);
     $.ajax({
         timeout: 30000,
-        url: preferences.serverURL + "?logout=" + token,
+        url: preferences.serverURL + "?logout=" + token+"&auth="+mediaController.ip_token,
         success: function (data) {
             if (uiController.savePlaylist)
                 uiController.toggleSavePlaylist();
         }
     })
+    accountController.setCookie("loginToken",Base64.encode(""),0);
+    accountController.setCookie("userName",Base64.encode(""),0);
     accountController.loggedIn = false;
     $('#popupLogin').popup('close');
     uiController.styleTopButtons();
@@ -57,6 +112,44 @@ accountController.logout = function () {
      },500)*/
     accountController.requestid = 1;
 }
+
+accountController.loadUserData = function(){
+    var loadPlaylists = function(playlists){
+        if(playlists){
+
+            alert(playlists)
+            playlists = JSON.parse(playlists);
+            if (playlists && playlists.length > 0) {
+                //Remove duplicate Playlists
+                for (var i = 0; i < playlistController.playlists.length; i++) {
+                    for (var j = 0; j < playlists.length; j++) {
+                        if (playlists[j].gid == playlistController.playlists[i].gid) {
+                            playlistController.playlists[i].splice(i, 1);
+                            i = i - 1;
+                        } else if (playlists[j].name == playlistController.playlists[i].name) {
+                            playlistController.playlists[i].name = playlistController.playlists[i].name + " (2)";
+                        }
+
+                    }
+                }
+                playlistController.playlists = playlistController.playlists.concat(playlists)
+                if (playlistController.loadedPlaylistSongs.length > 0 && playlistController.loadedPlaylistSongs[0].isPlaylist) {
+                    $scope.safeApply();
+                    setTimeout(function () {
+                        $("#playlistview").listview('refresh');
+                        $("#playlistview").show();
+                        uiController.makePlayListSortable();
+                        setTimeout(function () {
+                            uiController.playListScroll.refresh();
+                        }, 150)
+                    }, 0)
+                }
+            }
+        }
+    }
+    accountController.loadPlaylists(loadPlaylists)
+}
+
 
 accountController.signIn = function () {
     var send = function (email, pw) {
@@ -72,6 +165,9 @@ accountController.signIn = function () {
                     accountController.loggedIn = true;
                     accountController.loginToken = MD5(data + md5pw);
                     accountController.userName = username;
+                    accountController.setCookie("loginToken",Base64.encode(accountController.loginToken),1);
+                    accountController.setCookie("userName",Base64.encode(accountController.userName),1);
+
                     var btn = $('#header .ui-btn.animated').removeClass("animated");
                     $('#popupLogin').popup('close');
                     uiController.styleTopButtons();
@@ -82,44 +178,11 @@ accountController.signIn = function () {
                     }, 500)
                     accountController.requestid = 1;
 
-
+                    accountController.loadUserData();
 
                 // accountController.savePlaylist(1,"TEST",0,"TESTDATEN");
 
-                    var loadPlaylists = function(playlists){
-                        if(playlists){
 
-                            alert(playlists)
-                            playlists = JSON.parse(playlists);
-                            if (playlists && playlists.length > 0) {
-                                //Remove duplicate Playlists
-                                for (var i = 0; i < playlistController.playlists.length; i++) {
-                                    for (var j = 0; j < playlists.length; j++) {
-                                        if (playlists[j].gid == playlistController.playlists[i].gid) {
-                                            playlistController.playlists[i].splice(i, 1);
-                                            i = i - 1;
-                                        } else if (playlists[j].name == playlistController.playlists[i].name) {
-                                            playlistController.playlists[i].name = playlistController.playlists[i].name + " (2)";
-                                        }
-
-                                    }
-                                }
-                                playlistController.playlists = playlistController.playlists.concat(playlists)
-                                if (playlistController.loadedPlaylistSongs.length > 0 && playlistController.loadedPlaylistSongs[0].isPlaylist) {
-                                    $scope.safeApply();
-                                    setTimeout(function () {
-                                        $("#playlistview").listview('refresh');
-                                        $("#playlistview").show();
-                                        uiController.makePlayListSortable();
-                                        setTimeout(function () {
-                                            uiController.playListScroll.refresh();
-                                        }, 150)
-                                    }, 0)
-                                }
-                            }
-                        }
-                    }
-                    accountController.loadPlaylists(loadPlaylists)
 
 
                 }
