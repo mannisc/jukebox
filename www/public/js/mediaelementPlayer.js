d724cfcfcc49b30bc63b327810e5f932aece3645
masch <iggels@freenet.de>
2014-04-07 12:06:26 +0200

    mediaelementplayer

---
+++ www/public/js/mediaelementPlayer.js
@@ -0,0 +1,51 @@
+/**
+ * mediaelementPlayer.
+ *
+ * >>Description<<
+ *
+ * @author Manfred
+ * @date 07.04.14 - 12:00
+ * @copyright munichDev UG
+ */
+
+
+var mediaelementPlayer = function () {
+
+};
+
+
+
+
+mediaelementPlayer.prev = function () {
+    alert("prev")
+
+};
+
+
+mediaelementPlayer.playpause = function () {
+    alert("playpause")
+
+};
+
+
+mediaelementPlayer.stop = function () {
+    alert("stop")
+
+};
+
+
+mediaelementPlayer.next = function () {
+    alert("next")
+
+
+};
+
+mediaelementPlayer.mute = function () {
+    alert("mute")
+
+
+};
+
+mediaelementPlayer.fullscreen = function () {
+    alert("fullscreen")
+};
