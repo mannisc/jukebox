eae5c79dd8f2dbcf94410fc3e46ac65943f40684
manfred.schmidbartl <manfred.schmidbartl@munichdev.com>
2014-04-07 13:17:02 +0200

    embedded

--- www/public/js/embeddedPlayer.js
+++ www/public/js/embeddedPlayer.js
@@ -12,3 +12,35 @@ var embeddedPlayer = function () {
 
 };
 
+
+
+
+embeddedPlayer.playpause = function () {
+    alert("playpause")
+
+};
+
+
+embeddedPlayer.stop = function () {
+    alert("stop")
+
+};
+
+
+embeddedPlayer.mute = function () {
+    alert("mute")
+
+
+};
+
+embeddedPlayer.fullscreen = function () {
+    alert("fullscreen")
+};
+
+
+embeddedPlayer.setCurrentTime = function(percentage){
+
+    //Set progress in videoController
+    videoController.setProgressPercentage(percentage)
+
+}
