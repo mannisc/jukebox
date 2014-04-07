f044989baaa7f23ca41c87b0c6a8cc86c30c4754
masch <iggels@freenet.de>
2014-04-07 12:51:38 +0200

    time rail

--- www/public/js/videoController.js
+++ www/public/js/videoController.js
@@ -37,40 +37,41 @@ videoController.sharesocialEnabled = true;
  * Init the videoControlls, bind events
  */
 videoController.init = function(){
-    var controls =  $(".videoControlElements-controls");
 
-    controls.find(".videoControlElements-prevtrack-button").click(function () {
+    videoController.controls =  $(".videoControlElements-controls");
+
+    videoController.controls.find(".videoControlElements-prevtrack-button").click(function () {
         if(videoController.prevEnabled)
             alert("prev")
     })
 
-    controls.find(".videoControlElements-playpause-button").click(function () {
+    videoController.controls.find(".videoControlElements-playpause-button").click(function () {
         if(videoController.playpauseEnabled&&videoController.videoPlayer)
             videoController.videoPlayer.playpause();
 
     })
 
-    controls.find(".videoControlElements-stop-button").click(function () {
+    videoController.controls.find(".videoControlElements-stop-button").click(function () {
         if(videoController.stopEnabled&&videoController.videoPlayer)
             videoController.videoPlayer.stop();
     })
 
 
-    controls.find(".videoControlElements-nexttrack-button").click(function () {
+    videoController.controls.find(".videoControlElements-nexttrack-button").click(function () {
         if(videoController.nextEnabled)
             alert("next")
 
     })
 
 
-    controls.find(".videoControlElements-shuffle-button").click(function () {
+    videoController.controls.find(".videoControlElements-shuffle-button").click(function () {
         if(videoController.shuffleEnabled)
          alert("shuffle")
     })
 
-    controls.find(".videoControlElements-time-rail").click(function (event) {
+    videoController.controls.find(".videoControlElements-time-rail").click(function (event) {
         if(videoController.timerailEnabled) {
-            var total = controls.find('.videoControlElements-time-total'),
+            var total =  videoController.controls.find('.videoControlElements-time-total'),
                 //loaded  = controls.find('.videoControlElements-time-loaded'),
                 //current  = controls.find('.videoControlElements-time-current'),
                 //handle  = controls.find('.videoControlElements-time-handle'),
@@ -93,42 +94,69 @@ videoController.init = function(){
     })
 
 
-    controls.find(".videoControlElements-volume-button").click(function () {
+    videoController.controls.find(".videoControlElements-volume-button").click(function () {
         if(videoController.volumeEnabled&&videoController.videoPlayer)
             videoController.videoPlayer.mute();
     })
 
-    controls.find(".videoControlElements-volume-button").mouseover(function () {
+    videoController.controls.find(".videoControlElements-volume-button").mouseover(function () {
         if(videoController.volumeEnabled)
-          controls.find(".videoControlElements-volume-slider").show();
+            videoController.controls.find(".videoControlElements-volume-slider").show();
     })
 
-    controls.find(".videoControlElements-volume-button").mouseout(function () {
-        controls.find(".videoControlElements-volume-slider").hide();
+    videoController.controls.find(".videoControlElements-volume-button").mouseout(function () {
+        videoController.controls.find(".videoControlElements-volume-slider").hide();
     })
 
 
-    controls.find(".videoControlElements-fullscreen-button").click(function () {
+    videoController.controls.find(".videoControlElements-fullscreen-button").click(function () {
         if(videoController.fullscreenEnabled&&videoController.videoPlayer)
             videoController.videoPlayer.fullscreen();
     })
 
 
-    controls.find(".videoControlElements-button-choose-version").click(function () {
+    videoController.controls.find(".videoControlElements-button-choose-version").click(function () {
         if(videoController.versionsEnabled)
         mediaController.getVersions();
     })
 
-    controls.find(".videoControlElements-button-lyrics").click(function () {
+    videoController.controls.find(".videoControlElements-button-lyrics").click(function () {
         if(videoController.lyricsEnabled)
 
             mediaController.toggleLyrics();
     })
 
-    controls.find(".videoControlElements-button-facebook").click(function () {
+    videoController.controls.find(".videoControlElements-button-facebook").click(function () {
         if(videoController.sharesocialEnabled)
 
             mediaController.postOnFacebook();
     })
+
+}
+
+
+/**
+ * Set Progress in percentage
+ */
+videoController.setProgressPercentage = function(percentage){
+
+    var total =  videoController.controls.find('.videoControlElements-time-total');
+
+    videoController.controls.find(".videoControlElements-time-current").css("width",total.width()*percentage)
+
+}
+
+/**
+ * Set Progress in seconds
+ */
+videoController.setProgressTime = function(){
+
+
+}
+
+/**
+ * Set song length in seconds
+ */
+videoController.setMaxTime = function(){
 
 }
