7820edfc70c5d00a5d7b65f7f23b3c7b2cedce20
masch <iggels@freenet.de>
2014-04-07 09:28:05 +0200

    created files

---
+++ platforms/android/assets/www/public/js/embeddedPlayer.js
@@ -0,0 +1,14 @@
+/**
+ * embeddedPlayer.
+ *
+ * >>Description<<
+ *
+ * @author Manfred
+ * @date 07.04.14 - 09:25
+ * @copyright munichDev UG
+ */
+
+var embeddedPlayer = function () {
+
+};
+
---
+++ www/public/js/videoController.js
@@ -0,0 +1,16 @@
+/**
+ * videoController.
+ *
+ * >>Description<<
+ *
+ * @author Manfred
+ * @date 07.04.14 - 09:12
+ * @copyright munichDev UG
+ */
+
+
+
+
+var videoController = function () {
+
+};
