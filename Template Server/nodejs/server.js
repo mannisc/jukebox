f94f8dbe62719bbaeebe848d1b024c2e51c1963e
manfred.schmidbartl <manfred.schmidbartl@munichdev.com>
2014-05-04 19:24:57 +0200

    charts server

--- Template Server/nodejs/server.js
+++ Template Server/nodejs/server.js
@@ -1,8 +1,127 @@
 var express = require('express');
 var app = express();
 
-app.get('/', function(req, res){
-  res.send('hello world');
-});
+var ftpHandler = require('./ftpHandler');
+var httpHandler = require('./httpHandler');
+var chartsHandler = require('./chartsHandler');
+var templateHandler = require('./templateHandler');
 
-app.listen(3000);
+
+//Songbase FTP Properties
+var ftpProperties = {
+    host: "songbase.net",
+    user: "u76604889",
+    password: "songbasetwinners"
+}
+
+
+//This Callbacks should be called one after another
+
+
+//Connect FTP
+ftpHandler.connect(ftpProperties, onFTPConnection);
+
+//Connected
+function onFTPConnection() {
+    console.log("FTP Connected")
+
+    ftpHandler.downloadFile("/test/public/js/generatedData.template.js", "generatedData.template.js", onFTPDownloadTemplates)
+}
+
+//Templates Downloaded
+function onFTPDownloadTemplates() {
+    console.log("Templates Downloaded")
+
+    chartsHandler.update(httpHandler, "019c7bcfc5d37775d1e7f651d4c08e6f", 6, onChartsUpdated);
+
+}
+
+
+//Charts Updated
+function onChartsUpdated(tracks) {
+
+
+    console.log("Charts Updated: " + tracks.length + " Tracks")
+
+
+    var generatedDataTemplate = chartsHandler.fs.readFileSync("generatedData.template.js", "utf-8");
+
+    if (generatedDataTemplate && generatedDataTemplate != "") {
+
+        var templateProp = [
+            {
+                search: "insert_songcharts",
+                replace: JSON.stringify(tracks)
+            }
+        ]
+
+        var generatedData = templateHandler.buildTemplate(generatedDataTemplate, templateProp)
+        chartsHandler.fs.writeFileSync("generatedData.js",generatedData);
+        ftpHandler.uploadFile("/test/public/js/generatedData.js", "generatedData.js", onFTPUploadTemplates)
+
+    }
+
+
+}
+
+
+function onFTPUploadTemplates() {
+
+    console.log("Templates Uploaded")
+
+}
+
+
+/*
+ app.get('/', function(req, res){
+ res.send('Template Server');
+ });
+
+ app.listen(3000);
+
+
+
+ */
+
+//process.exit();
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ Template Server/nodejs/templateHandler.js
@@ -0,0 +1,31 @@
+/**
+ * templateHandler.
+ *
+ * >>Description<<
+ *
+ * @author Manfred
+ * @date 04.05.14 - 18:07
+ * @copyright munichDev UG
+ */
+
+
+var templateHandler = function () {
+}
+
+
+
+
+templateHandler.buildTemplate = function(templateText,templateProps) {
+
+    if(templateProps&&templateProps.length>0){
+        for (var i = 0; i < templateProps.length; i++) {
+            var templateProp = templateProps[i]
+            templateText = templateText.replace(templateProp.search,templateProp.replace)
+        }
+    }
+    return  templateText;
+
+}
+
+
+module.exports = templateHandler;
