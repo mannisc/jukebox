a8312e5117bb9019c1316e2238e4399d85b6d420
Norbert <nschmidbartl@gmx.de>
2014-05-28 04:00:38 +0200

    merge

--- www/public/js/importController.js
+++ www/public/js/importController.js
@@ -13,8 +13,29 @@ importController.importPlaylist = function(url){
     $.ajax({
         url: preferences.serverURL + "?importplaylist="+encodeURIComponent(url)+"&auth="+authController.ip_token,
         success: function (data) {
+          alert();
            console.dir("imported playlist:");
            console.dir(data);
+           var playlist = [];
+            console.dir("current playlist:");
+            console.dir(playlistController.getLoadedPlaylist());
+
+            for (var j = 0; j <  data.track.length; j++) {
+                playlist[j] = {
+                    artist: unescape(data.track[j].artist),
+                    title: unescape(data.track[j].title),
+                    gid: playlistController.getNewID(),
+                    id: "plsid" + helperFunctions.padZeros(j, ("" + playlistController.loadedPlaylistSongs.length).length)
+                }
+            }
+
+            playlistController.addSongsToPlaylist(playlistController.getLoadedPlaylist(), playlist)
+
+
+
+            console.dir("new playlist:");
+
+            console.dir(playlistController.getLoadedPlaylist());
         },
         error: function (xhr, ajaxOptions, thrownError) {
             console.dir("imported playlist - error:");
